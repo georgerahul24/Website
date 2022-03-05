d4693782960b1edb918107cb9b9fc9aecca7d8e4
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-03-05 15:13:48 +0530

    Updated contents

--- Pages/Biography.js
+++ Pages/Biography.js
@@ -5,12 +5,11 @@ var content='';
 function nextMember(value) {
   i=i+value;
   if(i==0){
-    content="Welcome!";
+    content="Welcome! Click the arrows to proceed...";
     document.getElementById("navbutton2").disabled = true;
   };
   if (i==1){
-     content="Click the arrows to proceed"
-     ;
+     content="READY? Lets Start....";
    };
  if (i==2){
     content="George Rahul was born on September 24,2004 as a child to Rupesh George and Sajitha TA in Lucknow,UP.";
@@ -19,10 +18,10 @@ function nextMember(value) {
     content="As a child itself, George was very capable of high level reaasoning as well as to grasp very high level concepts";
   };
   if (i==4){
-    content="By the age of 10, he was well versed in all the subjects better than the reeaserchers and sceintists of his time";
+    content="By the age of 10, he was well versed in all the subjects better than the researchers and sceintists of his time";
   };
   if (i==5){
-    content="The END";
+    content="To be CONTINUED";
   };
   document.getElementById("snippetPara").innerHTML =content;
 }
